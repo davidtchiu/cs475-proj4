f426faec16b6f421ab60151bf6685ab965d6ec82
David Chiu <david.t.chiu@gmail.com>
2022-05-07 23:02:48 -0700

    removed old main.c

--- include/deadlock.h
+++ include/deadlock.h
@@ -1,3 +1,4 @@
-//TODO - add your deadlock function prototypes
+// TODO - add your deadlock function prototypes
 
-//TODO - add an "extern" declaration for the RAG
+// TODO - add an "extern" declaration for the RAG.
+// This *should work* extern int32 rag[NLOCK + NPROC][NLOCK + NPROC];
--- system/main.c
+++ system/main.c
@@ -2,22 +2,86 @@
 
 #include <xinu.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-mutex_t	lock = FALSE;
-void    printchar(char c)
+#define N 5 // number of philosophers and forks
+
+// TODO - locks must be declared and initialized here
+mutex_t lock = FALSE;
+mutex_t fork[N] = {FALSE, FALSE, FALSE, FALSE, FALSE};
+
+/**
+ * Delay for a random amount of time
+ * @param alpha delay factor
+ */
+void holdup(int32 alpha)
 {
-    int i;
-    mutex_lock(&lock);
-    for (i=0; i<10; i++)
-        kprintf("%c", c);
-    mutex_unlock(&lock);
+	long delay = rand() * alpha;
+	while (delay-- > 0)
+		; // no op
+}
+
+/**
+ * Eat for a random amount of time
+ */
+void eat()
+{
+	holdup(10000);
+}
+
+/**
+ * Think for a random amount of time
+ */
+void think()
+{
+	holdup(1000);
+}
+
+/**
+ * Philosopher's code
+ * @param phil_id philosopher's id
+ */
+void philosopher(uint32 phil_id)
+{
+	uint32 right = phil_id;										 // right fork
+	uint32 left = N - ((N - phil_id) % N) - 1; // left fork
+
+	while (TRUE)
+	{
+		// think 70% of the time
+		if (rand() % 10 < 7)
+		{
+			mutex_lock(&lock);
+			kprintf("Philosopher %d thinking: zzzzzZZZz\n", phil_id);
+			mutex_unlock(&lock);
+
+			think();
+		}
+		else // eat 30% of the time
+		{
+			mutex_lock(&fork[right]); // grab the right fork (or wait)
+			mutex_lock(&fork[left]);	// grab the left fork (or wait)
+
+			mutex_lock(&lock);
+			kprintf("Philosopher %d eating: nom nom nom\n", phil_id);
+			mutex_unlock(&lock);
+
+			eat();
+
+			mutex_unlock(&fork[left]);
+			mutex_unlock(&fork[right]);
+		}
+	}
+}
 }
 
 int main(uint32 argc, uint32 *argv)
 {
-    //priority of process is input as the 3rd argument of create()
-    ready(create((void*) printchar, INITSTK, 15, "P1", 1, 'A'), FALSE);
-    ready(create((void*) printchar, INITSTK, 15, "P2", 1, 'B'), FALSE);
+	ready(create((void *)philosopher, INITSTK, 15, "Ph1", 1, 0), FALSE);
+	ready(create((void *)philosopher, INITSTK, 15, "Ph2", 1, 1), FALSE);
+	ready(create((void *)philosopher, INITSTK, 15, "Ph3", 1, 2), FALSE);
+	ready(create((void *)philosopher, INITSTK, 15, "Ph4", 1, 3), FALSE);
+	ready(create((void *)philosopher, INITSTK, 15, "Ph5", 1, 4), FALSE);
 
-    return 0;
+	return 0;
 }
